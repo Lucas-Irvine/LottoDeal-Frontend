3c34dc2ffa9229478267d33ce8622f89c6057229
Dominic Whyte <Whyte@nat-oitwireless-inside-vapornet100-d-23871.Princeton.EDU>
2017-04-07 11:57:30 -0400

    test

--- js/login.js
+++ js/login.js
@@ -1,6 +1,33 @@
 var app = angular.module("login_app", []); // put any module dependencies here ["ngRoute"]
 
 // app.config(function($routeProvider)) { can put routing here}
+ window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '228917890846081',
+          xfbml      : true,
+          version    : 'v2.8'
+        });
+        FB.AppEvents.logPageView();
+      };
+
+      (function(d, s, id){
+         var js, fjs = d.getElementsByTagName(s)[0];
+         if (d.getElementById(id)) {return;}
+         js = d.createElement(s); js.id = id;
+         js.src="https://connect.facebook.net/en_US/all.js";
+         fjs.parentNode.insertBefore(js, fjs);
+       }(document, 'script', 'facebook-jssdk'));
+      
+
+FB.getLoginStatus(function(response) {
+  if (response.status === 'connected') {
+    console.log('Logged in.');
+  }
+  else {
+    FB.login();
+  }
+});
+
 
 app.controller("loginController", function($scope) {
 	$scope.selectedTab = 0;
