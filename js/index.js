ebd3e5a46a8ae9ba2ee33e880b6bdee2e3366260
Dominic Whyte <dwhyte@princeton.edu>
2017-04-14 11:36:51 -0400

    test

--- js/index.js
+++ js/index.js
@@ -17,7 +17,10 @@ app.controller("indexController", function($scope) {
     	success: function(data) {
             var items = JSON.parse(data)
             for (i = 0; i < items.length; i++) {
-                items[i].testnum = 50
+                items[i].percentageRaised = items[i].amountRaised / items[i].price;
+                var expirationDate = new Date(items[i].expirationDate);
+                var date = new Date();
+                items[i].expirationDate = DateDiff.inDays(date, expirationDate) + " Days left";
             }
             $scope.posts = items;
             console.log($scope.posts)
@@ -64,6 +67,37 @@ app.controller("indexController", function($scope) {
 }
 
 })	
+
+//Code modified from http://ditio.net/2010/05/02/javascript-date-difference-calculation/
+var DateDiff = {
+ 
+    inDays: function(d1, d2) {
+        var t2 = d2.getTime();
+        var t1 = d1.getTime();
+ 
+        return parseInt((t2-t1)/(24*3600*1000));
+    },
+ 
+    inWeeks: function(d1, d2) {
+        var t2 = d2.getTime();
+        var t1 = d1.getTime();
+ 
+        return parseInt((t2-t1)/(24*3600*1000*7));
+    },
+ 
+    inMonths: function(d1, d2) {
+        var d1Y = d1.getFullYear();
+        var d2Y = d2.getFullYear();
+        var d1M = d1.getMonth();
+        var d2M = d2.getMonth();
+ 
+        return (d2M+12*d2Y)-(d1M+12*d1Y);
+    },
+ 
+    inYears: function(d1, d2) {
+        return d2.getFullYear()-d1.getFullYear();
+    }
+}
 
 // /* Create Tabs */
 
