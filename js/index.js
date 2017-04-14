b67bccd74bb246fdffa7c358ec005244d06a185a
Dominic Whyte <dwhyte@princeton.edu>
2017-04-14 11:45:02 -0400

    test

--- js/index.js
+++ js/index.js
@@ -17,11 +17,11 @@ app.controller("indexController", function($scope) {
     	success: function(data) {
             var items = JSON.parse(data)
             for (i = 0; i < items.length; i++) {
-                items[i].percentageRaised = Number(items[i].amountRaised) / Number(items[i].price);
+                items[i].percentageRaised = (Number(items[i].amountRaised) / Number(items[i].price)) * 100;
                 console.log( "Raised" + items[i].percentageRaised);
                 var expirationDate = new Date(items[i].expirationDate);
                 var date = new Date();
-                items[i].expirationDate = DateDiff.inDays(date, expirationDate) + " Days left";
+                items[i].expirationDate = DateDiff.inHours(date, expirationDate) + " Hours " + DateDiff.inDays(date, expirationDate) + " Days left";
             }
             $scope.posts = items;
             console.log($scope.posts)
@@ -71,7 +71,14 @@ app.controller("indexController", function($scope) {
 
 //Code modified from http://ditio.net/2010/05/02/javascript-date-difference-calculation/
 var DateDiff = {
+
+    inHours: function(d1, d2) {
+        var t2 = d2.getTime();
+        var t1 = d1.getTime();
  
+        return parseInt((t2-t1)/(3600*1000));
+    },
+
     inDays: function(d1, d2) {
         var t2 = d2.getTime();
         var t1 = d1.getTime();
@@ -79,25 +86,25 @@ var DateDiff = {
         return parseInt((t2-t1)/(24*3600*1000));
     },
  
-    inWeeks: function(d1, d2) {
-        var t2 = d2.getTime();
-        var t1 = d1.getTime();
+    // inWeeks: function(d1, d2) {
+    //     var t2 = d2.getTime();
+    //     var t1 = d1.getTime();
  
-        return parseInt((t2-t1)/(24*3600*1000*7));
-    },
+    //     return parseInt((t2-t1)/(24*3600*1000*7));
+    // },
  
-    inMonths: function(d1, d2) {
-        var d1Y = d1.getFullYear();
-        var d2Y = d2.getFullYear();
-        var d1M = d1.getMonth();
-        var d2M = d2.getMonth();
+    // inMonths: function(d1, d2) {
+    //     var d1Y = d1.getFullYear();
+    //     var d2Y = d2.getFullYear();
+    //     var d1M = d1.getMonth();
+    //     var d2M = d2.getMonth();
  
-        return (d2M+12*d2Y)-(d1M+12*d1Y);
-    },
+    //     return (d2M+12*d2Y)-(d1M+12*d1Y);
+    // },
  
-    inYears: function(d1, d2) {
-        return d2.getFullYear()-d1.getFullYear();
-    }
+    // inYears: function(d1, d2) {
+    //     return d2.getFullYear()-d1.getFullYear();
+    // }
 }
 
 // /* Create Tabs */
