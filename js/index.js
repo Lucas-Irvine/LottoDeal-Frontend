310c3aa16ca39c45aef405c7c8c255aebb4e000d
Dominic Whyte <Whyte@nat-oitwireless-inside-vapornet100-d-23871.Princeton.EDU>
2017-04-13 21:20:12 -0400

    test

--- js/index.js
+++ js/index.js
@@ -26,6 +26,13 @@ app.controller("indexController", function($scope) {
     	}
     });
 
+    $(".bidButton").click(function(e) {
+        console.log(this)
+
+        idattr = this.attr("id");
+        console.log(idattr)
+    })
+
 })	
 
 // /* Create Tabs */
