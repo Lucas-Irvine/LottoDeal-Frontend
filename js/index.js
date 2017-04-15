8d8bb3b7fd29e4a25d7e00dd07300329b8f638bd
Antony Toron <atoron@princeton.edu>
2017-04-15 06:46:12 -0400

    testing angular routing

--- js/index.js
+++ js/index.js
@@ -80,20 +80,21 @@ app.controller("indexController", ["$scope", "$rootScope", "$location", function
         // can make this a param in url instead of part of path (?itemId = 123)
 
         // if item is in database, display that webpage
-        for (var i = 0; i < $scope.posts.length; i++) {
-            var post = $scope.posts[i]
-            var postId = post["_id"]
-            if (path == postId) {
-                targetPost = post;
-                foundItem = true;
-                break;
-            }
-        }
+        // for (var i = 0; i < $scope.posts.length; i++) {
+        //     var post = $scope.posts[i]
+        //     var postId = post["_id"]
+        //     if (path == postId) {
+        //         targetPost = post;
+        //         foundItem = true;
+        //         break;
+        //     }
+        // }
 
-        // otherwise, go back to home page
-        if (!foundItem) {
-            $location.path('/')
-        }
+        // // otherwise, go back to home page
+        // if (!foundItem) {
+        //     console.log("hi")
+        //     $location.path('/')
+        // }
     });
 
 
