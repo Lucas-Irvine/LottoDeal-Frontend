02afbc50a809a5f124bd0890785dc2bb292c3748
Lucas Irvine <lucasirvine@nat-oitwireless-inside-vapornet100-c-7561.Princeton.EDU>
2017-04-16 16:34:20 -0400

    bid changes

--- js/profile.js
+++ js/profile.js
@@ -1,104 +1,58 @@
 var app = angular.module("profile_app", ["ngRoute"])
 
-
 app.controller("profileController", ["$scope", "$rootScope", "$location", function($scope, $rootScope, $location) {
 	$scope.selectedTab = 0
 
-	$scope.posts = []
+	$scope.bids = []
 
-	var url = "https://localhost:8000/getPosts";
-
-
-    console.log('test')
+	var url = "https://localhost:8000/getBidsofUsers";
 
     // AJAX POST TO SERVER
+    var userID = localStorage.getItem("curUserID")
+    var dataGET = {
+        userID : userID
+    }
     $.ajax({
     	url: url,
-    	type: 'GET',
+        data: dataGET,
+        type: 'GET',
     	success: function(data) {
-            var items = JSON.parse(data)
-            for (i = 0; i < items.length; i++) {
-                items[i].percentageRaised = (Number(items[i].amountRaised) / Number(items[i].price)) * 100;
-                console.log( "Raised" + items[i].percentageRaised);
-                var expirationDate = new Date(items[i].expirationDate);
-                var date = new Date();
-                items[i].expirationDate = DateDiff.inHours(date, expirationDate) + " Hours " + DateDiff.inDays(date, expirationDate) + " Days left";
-            }
-            $scope.posts = items;
-            console.log($scope.posts)
+            var bids = JSON.parse(data)
+            $scope.bids = bids;
+            console.log($scope.bids)
             $scope.$apply()
         },
-        error: function(response, error) {
-          console.log(response)
-          console.log(error)
-      }
+        error: function(response,  error) {
+            console.log(response)
+            console.log(error)
+        }
     });
-    
+
+    // AJAX POST TO SERVER
+    var url = "https://localhost:8000/getBiddedItemsofUsers";
+    var userID = localStorage.getItem("curUserID")
+    var dataGET = {
+        userID : userID
+    }
+    console.log('Asking for notifications')
+    $.ajax({
+        url: notificationUrl,
+        data: dataGET,
+        type: 'GET',
+        success: function(data) {
+            var items = JSON.parse(data)
+            $scope.items = items;
+            console.log($scope.items)
+            $scope.$apply()
+        },
+        error: function(response,  error) {
+            console.log(response)
+            console.log(error)
+        }
+    });
+
 
     $scope.targetPost = null;
-
-
-
-    $scope.bid = function (event, amount, amountRaised, price) {
-        // DISPLAY BID ON FRONT-END
-        var progressbar = $("#progress-bar-" + event)
-        // console.log(progressbar)
-        var currentAmount = progressbar.css("width")
-        // console.log(currentAmount)
-        var totalWidth = (parseInt(currentAmount.substring(0, currentAmount.length - 2)) * parseInt(price)) / parseInt(amountRaised)
-        var percentage = progressbar.width() / progressbar.parent().width() * 100
-        var newAmount = parseInt(amountRaised) + parseInt(amount)
-        // console.log(newAmount)
-        var newPercent = ((newAmount * 1.0) / (parseInt(price) * 1.0))
-        // console.log(newPercent)
-        // var newWidth = progressbar.parent().width() * newPercent
-        var newWidth = totalWidth * newPercent
-        // console.log("newwidth: " + newWidth)
-        var pixelWidth = ""  + newWidth + "px"
-        progressbar.css("width", pixelWidth)
-
-        // change the amount raised
-        var amountText = $("#amountRaised-" + event)
-        // console.log(amountText)
-        amountText.text("$" + newAmount + " of $" + price + " raised")
-
-
-
-
-        // ADD BID TO DATABASE
-        console.log("Adding bid for item " + event + " for " + amount)
-
-
-        var url = "https://localhost:8000/addBid";
-        var userID = localStorage.getItem("curUserID")
-        if (userID != null) {
-            data = {
-               itemID: event,
-               userID: userID,
-               newAmount: Number(amount)
-           }
-
-           $.ajax({
-            url: url,
-            data: data,
-            type: 'POST',
-            success: function(data) {
-                console.log('Bid added for ' + event)
-            },
-            error: function(response, error) {
-                console.log(response)
-                console.log(error)
-            }
-        });
-
-       }
-       else {
-        console.log('UserID is null')
-    }
-
-}
-
-}])
 
 /* JS for the tabs */
 $(document).ready(function() {
