0cd05f620fac8599c96a97199433253cd02b4949
Lucas Irvine <lucasirvine@nat-oitwireless-inside-vapornet100-c-7561.Princeton.EDU>
2017-04-16 15:46:14 -0400

    Profile page

--- js/profile.js
+++ js/profile.js
@@ -98,17 +98,16 @@ app.controller("profileController", ["$scope", "$rootScope", "$location", functi
 
 }
 
-}])	
+}])
 
-
-
-
-
-
-
-
-
-
+/* JS for the tabs */
+$(document).ready(function() {
+    $(".btn-pref .btn").click(function () {
+        $(".btn-pref .btn").removeClass("btn-primary").addClass("btn-default");
+        // $(".tab").addClass("active"); // instead of this do the below
+        $(this).removeClass("btn-default").addClass("btn-primary");
+    });
+});
 
 
 // For Facebook login
