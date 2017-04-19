ee68ef789693ddad65789fe224da09e3a059f87d
Steven  Takeshita <Steven@nat-oitwireless-inside-vapornet100-d-8746.Princeton.EDU>
2017-04-18 23:33:33 -0400

    new changes pushed by script

--- js/sell.js
+++ js/sell.js
@@ -15,6 +15,7 @@ app.controller("sellController", function($scope, $http) {
 	    var date = new Date()
 	    var offset = $("#expirDate").val()
 //	    var sellerID = localStorage.getItem("curUserID")
+		var sellerID = "12345"
 	    if (offset == 1) {
 	    	date.setDate(date.getDate() + 1); 
 	    }
@@ -34,7 +35,7 @@ app.controller("sellController", function($scope, $http) {
 	    	title: title,
 	    	description: description,
 	    	expirationDate: date
-//	    	sellerID: sellerID
+	    	sellerID: sellerID
 	    	//image: image
 	    }
 
