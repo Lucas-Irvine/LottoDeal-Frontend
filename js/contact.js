4fe9d4671a183861fe960a2fcf6d896345941390
Dominic Whyte <dwhyte@princeton.edu>
2017-04-20 16:48:25 -0400

    contact

--- js/contact.js
+++ js/contact.js
@@ -12,3 +12,36 @@ function sendEmail() {
 console.log('testemail')
 emailjs.send("lotto_deal", "sample_template", {"email":"dwhyte@princeton.edu"})
 }
+
+
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '228917890846081',
+      xfbml      : true,
+      cookie     : true,
+      version    : 'v2.8'
+  });
+
+    // Check whether the user already logged in
+    FB.getLoginStatus(function(response) {
+        if (response.status === 'connected') {
+            //display user data
+            getFbUserData();
+
+            // Get and display the user profile data
+            document.getElementById('fbLink').setAttribute("onclick","fbLogout()");
+            document.getElementById('fbLink').innerHTML = 'Facebook Logout';
+        }
+        else {
+            console.log('Not logged in');
+        }
+    });
+};
+
+(function(d, s, id){
+ var js, fjs = d.getElementsByTagName(s)[0];
+ if (d.getElementById(id)) {return;}
+ js = d.createElement(s); js.id = id;
+ js.src="https://connect.facebook.net/en_US/all.js";
+ fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
