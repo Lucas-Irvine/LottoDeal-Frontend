cdacd6782d9b1e9eafd6667c02d47cb749749f81
Antony Toron <atoron@princeton.edu>
2017-04-20 16:49:02 -0400

    sell changes

--- js/sell.js
+++ js/sell.js
@@ -3,63 +3,63 @@ var app = angular.module("app", []);
 app.controller("sellController", function($scope, $http) {
 	console.log("got here")
 
-    $("#submitForm").submit(function(e) {
-    	console.log("got here")
-	    e.preventDefault();
+ //    $("#submitForm").submit(function(e) {
+ //    	console.log("got here")
+	//     e.preventDefault();
 
-    	var url = "https://localhost:8000/createPost";
+ //    	var url = "https://localhost:8000/createPost";
 
-	    var price = $("#price").val()
-	    var title = $("#title").val()
-	    var description = $("#description").val()
-	    var date = new Date()
-	    var offset = $("#expirDate").val()
-	    var sellerID = localStorage.getItem("curUserID")
-	    console.log(sellerID);
-	    if (offset == 1) {
-	    	date.setDate(date.getDate() + 1); 
-	    }
-	    else if (offset == 2) {
-	    	date.setDate(date.getDate() + 7); 
-	    }
-	    else {
-	    	date.setDate(date.getDate() + 30); 
-	    }
+	//     var price = $("#price").val()
+	//     var title = $("#title").val()
+	//     var description = $("#description").val()
+	//     var date = new Date()
+	//     var offset = $("#expirDate").val()
+	//     var sellerID = localStorage.getItem("curUserID")
+	//     console.log(sellerID);
+	//     if (offset == 1) {
+	//     	date.setDate(date.getDate() + 1); 
+	//     }
+	//     else if (offset == 2) {
+	//     	date.setDate(date.getDate() + 7); 
+	//     }
+	//     else {
+	//     	date.setDate(date.getDate() + 30); 
+	//     }
 
-	    //var image = $("#itemPicture").val()
+	//     //var image = $("#itemPicture").val()
 
-	   // console.log(image)
+	//    // console.log(image)
 
-	    data = {
-	    	price: price,
-	    	title: title,
-	    	description: description,
-	    	expirationDate: date,
-	    	sellerID: sellerID
-	    	//image: image
-	    }
+	//     data = {
+	//     	price: price,
+	//     	title: title,
+	//     	description: description,
+	//     	expirationDate: date,
+	//     	sellerID: sellerID
+	//     	//image: image
+	//     }
 
-	    // var formData = new FormData($(this)[0]);
-	    // var data = formData
-	    // console.log(formData)
+	//     // var formData = new FormData($(this)[0]);
+	//     // var data = formData
+	//     // console.log(formData)
 
-	    // AJAX POST TO SERVER
-	    $.ajax({
-		    url: url,
-		    type: 'POST',
-		    data: data,
-		    //data: new FormData(this),
-		    success: function(data) {
-				console.log(data)
-		    },
-		    //contentType: false,
-		    //processData: false,
-		    error: function(response, error) {
-				console.log(response)
-				console.log(error)
-		    }
-		});
+	//     // AJAX POST TO SERVER
+	//     $.ajax({
+	// 	    url: url,
+	// 	    type: 'POST',
+	// 	    data: data,
+	// 	    //data: new FormData(this),
+	// 	    success: function(data) {
+	// 			console.log(data)
+	// 	    },
+	// 	    //contentType: false,
+	// 	    //processData: false,
+	// 	    error: function(response, error) {
+	// 			console.log(response)
+	// 			console.log(error)
+	// 	    }
+	// 	});
 
-	});
+	// });
 })
 
