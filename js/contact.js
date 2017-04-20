4929644ce39a2d33de1a8e6b9d24d2f54efc3944
Dominic Whyte <dwhyte@princeton.edu>
2017-04-20 19:46:47 -0400

    test

--- js/contact.js
+++ js/contact.js
@@ -43,34 +43,50 @@ function showLoginPopup() {
 
 var facebookLoginButton = document.getElementById("loginToFacebook");
 
-var facebookLoginButton = document.getElementById("loginToFacebook");
-
 // When the user clicks the button, open the modal 
 facebookLoginButton.onclick = function() {
     console.log('logging in/out')
     FB.getLoginStatus(function(response) {
         if (response.status === 'connected') {
             //display user data
-            FB.logout(function() {
-                facebookLoginButton.setAttribute("onclick", "fbLogin()");
-                facebookLoginButton.innerHTML = 'Facebook Login';
-            });
+            fbLogout()
         } else {
-            FB.login(function(response) {
-                if (response.authResponse) {
-                    // Get and display the user profile data
-                    facebookLoginButton.setAttribute("onclick", "fbLogout()");
-                    facebookLoginButton.innerHTML = 'Facebook Logout';
-                    //getFbUserData();
-                    //window.focus();
-                } else {
-                    console.log('User cancelled login or did not fully authorize.');
-                }
-            }, {
-                scope: 'email'
-            });
+            fbLogin()
         }
     });
+}
+
+function fbLogin() {
+    var window = FB.login(function (response) {
+        if (response.authResponse) {
+            // Get and display the user profile data
+            facebookLoginButton.setAttribute("onclick","fbLogout()");
+            facebookLoginButton.innerHTML = 'Facebook Logout';
+            getFbUserData();
+        } else {
+            document.getElementById('status').innerHTML = 'User cancelled login or did not fully authorize.';
+        }
+    }, {scope: 'email'});
+}
+
+// Logout from facebook
+function fbLogout() {
+    //delete local storage
+    delete localStorage.curUserID;
+
+    FB.logout(function() {
+        facebookLoginButton.setAttribute("onclick","fbLogin()");
+        facebookLoginButton.innerHTML = 'Facebook Login';
+    });
+}
+
+function getFbUserData(){
+    FB.api('/me', {locale: 'en_US', fields: 'id,first_name,last_name,email,link,gender,locale,picture'},
+        function (response) {
+            localStorage.setItem("curUserID", response.id);
+            // Save user data
+            saveUserData(response);
+        });
 }
 //End Facebook login code -----------------------------------
 
