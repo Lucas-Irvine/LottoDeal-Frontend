d90eb5a99de04e1fc62547ce62d87a7a34555aaf
Dominic Whyte <dwhyte@princeton.edu>
2017-04-21 16:56:44 -0400

    test

--- js/item.js
+++ js/item.js
@@ -3,8 +3,10 @@ var app = angular.module("item_app", ["ngRoute"])
 app.controller("itemController", ["$scope", "$rootScope", "$location", function($scope, $rootScope, $location) {
     var searchObject = $location.search();
     var id = searchObject['id'];
+    console.log(id);
+
     console.log(searchObject);
 
-
+    console.log($location.search('id'));
 }]) 
 
