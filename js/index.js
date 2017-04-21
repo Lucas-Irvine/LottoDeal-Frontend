39d05e9f8a937ce58d47e6933d0187c87963adc9
Dominic Whyte <dwhyte@princeton.edu>
2017-04-21 11:15:50 -0400

    test

--- js/index.js
+++ js/index.js
@@ -185,6 +185,32 @@ app.controller("indexController", ["$scope", "$rootScope", "$location", function
         amountText.text("$" + newAmount + " of $" + price + " raised")
 
 
+        console.log('activate stripe ')
+
+        var handler = StripeCheckout.configure({
+            key: 'pk_test_I1JByOdv34UVHxZhjKYlKGc4',
+            image: 'https://stripe.com/img/documentation/checkout/marketplace.png',
+            locale: 'auto',
+            token: function(token) {
+                // You can access the token ID with `token.id`.
+                // Get the token ID to your server-side code for use.
+            }
+        });
+
+        document.getElementById('customButton').addEventListener('click', function(e) {
+            // Open Checkout with further options:
+            handler.open({
+                name: 'LottoDeal',
+                description: '2 widgets',
+                amount: 2000
+            });
+            e.preventDefault();
+        });
+
+        // Close Checkout on page navigation:
+        window.addEventListener('popstate', function() {
+            handler.close();
+        });
 
 
 
