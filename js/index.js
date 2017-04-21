a76aba06d025d78798e78441511f1f2237324ed1
Dominic Whyte <dwhyte@princeton.edu>
2017-04-21 11:20:09 -0400

    test

--- js/index.js
+++ js/index.js
@@ -159,6 +159,15 @@ app.controller("indexController", ["$scope", "$rootScope", "$location", function
     //     }
     // });
 
+    var handler = StripeCheckout.configure({
+            key: 'pk_test_I1JByOdv34UVHxZhjKYlKGc4',
+            image: 'https://stripe.com/img/documentation/checkout/marketplace.png',
+            locale: 'auto',
+            token: function(token) {
+                // You can access the token ID with `token.id`.
+                // Get the token ID to your server-side code for use.
+            }
+    });
 
 
     $scope.bid = function (event, amount, amountRaised, price) {
@@ -187,30 +196,14 @@ app.controller("indexController", ["$scope", "$rootScope", "$location", function
 
         console.log('activate stripe ')
 
-        var handler = StripeCheckout.configure({
-            key: 'pk_test_I1JByOdv34UVHxZhjKYlKGc4',
-            image: 'https://stripe.com/img/documentation/checkout/marketplace.png',
-            locale: 'auto',
-            token: function(token) {
-                // You can access the token ID with `token.id`.
-                // Get the token ID to your server-side code for use.
-            }
-        });
-
-        document.getElementById('customButton').addEventListener('click', function(e) {
-            // Open Checkout with further options:
-            handler.open({
+        
+        handler.open({
                 name: 'LottoDeal',
                 description: '2 widgets',
                 amount: 2000
             });
-            e.preventDefault();
-        });
-
-        // Close Checkout on page navigation:
-        window.addEventListener('popstate', function() {
-            handler.close();
-        });
+        e.preventDefault();
+        
 
 
 
