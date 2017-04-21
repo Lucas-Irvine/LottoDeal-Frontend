299c64b77871be64dc07e8835cfb300eb0d0b959
Dominic Whyte <dwhyte@princeton.edu>
2017-04-21 11:39:34 -0400

    test

--- js/index.js
+++ js/index.js
@@ -159,16 +159,29 @@ app.controller("indexController", ["$scope", "$rootScope", "$location", function
     //     }
     // });
 
-    var handler = StripeCheckout.configure({
-            key: 'pk_test_I1JByOdv34UVHxZhjKYlKGc4',
-            image: 'https://stripe.com/img/documentation/checkout/marketplace.png',
-            locale: 'auto',
-            token: function(token) {
-                // You can access the token ID with `token.id`.
-                // Get the token ID to your server-side code for use.
-            }
-    });
 
+var handler = StripeCheckout.configure({
+    key: 'pk_test_I1JByOdv34UVHxZhjKYlKGc4',
+    image: 'https://stripe.com/img/documentation/checkout/marketplace.png',
+    locale: 'auto',
+    token: function(token) {
+        data = {
+            stripeToken: token
+        }
+        $.ajax({
+            url: 'https://localhost:8000/performPayment',
+            data: data,
+            type: 'POST',
+            success: function(data) {
+                console.log('success payment')
+            },
+            error: function(response, error) {
+                console.log(response)
+                console.log(error)
+            }
+        });
+    }
+});
 
     $scope.bid = function (event, amount, amountRaised, price) {
         // DISPLAY BID ON FRONT-END
