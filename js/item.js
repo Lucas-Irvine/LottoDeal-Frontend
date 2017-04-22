cfed8ecb9ae06c81a3917fc649a056b2f97ed28e
Antony Toron <atoron@princeton.edu>
2017-04-21 20:27:23 -0400

    test

--- js/item.js
+++ js/item.js
@@ -25,7 +25,40 @@ app.controller("itemController", ["$scope", "$rootScope", "$location", "$routePa
             var parsed = JSON.parse(data)
             console.log(parsed)
 
+            // amount raised
+            parsed.percentageRaised = (Number(parsed.amountRaised) / Number(parsed.price)) * 100;
+            console.log( "Raised" + parsed.percentageRaised);
+            var expirationDate = new Date(parsed.expirationDate);
+            var date = new Date();
+            parsed.expirationDate = DateDiff.inHours(date, expirationDate) + " Hours " + DateDiff.inDays(date, expirationDate) + " Days left";
+
+
+
+
+            var image = parsed.img;
+            if (image == null) {
+            	parsed["src"] = "http://placehold.it/320x150"
+            }
+            else {
+            	console.log(items[i])
+                console.log(items[i].img.data)
+
+                var raw = String.fromCharCode.apply(null, items[i].img.data.data)
+
+                var b64=btoa(raw)
+                var dataURL = "data:image/jpeg;base64," + b64;
+
+                items[i]["src"] = dataURL;
+            }
+
+
             $scope.post = parsed;
+
+
+
+
+
+
             $scope.$apply();
         },
         error: function(response, error) {
@@ -33,6 +66,29 @@ app.controller("itemController", ["$scope", "$rootScope", "$location", "$routePa
           console.log(error)
       }
     });
+
+
+    //Code modified from http://ditio.net/2010/05/02/javascript-date-difference-calculation/
+	var DateDiff = {
+
+	    inHours: function(d1, d2) {
+	        var t2 = d2.getTime();
+	        var t1 = d1.getTime();
+	        if (t2 == null || t1 == null) {
+	            return 0
+	        }
+	        return (parseInt((t2-t1)/(3600*1000))) % 24;
+	    },
+
+	    inDays: function(d1, d2) {
+	        var t2 = d2.getTime();
+	        var t1 = d1.getTime();
+	        if (t2 == null || t1 == null) {
+	            return 0
+	        }
+	        return parseInt((t2-t1)/(24*3600*1000));
+	    }
+	};
 
 
     // console.log($routeParams)
