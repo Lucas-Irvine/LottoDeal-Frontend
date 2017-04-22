e8c6759851c4228f0a25cb517525b2c94b2131a7
Dominic Whyte <dwhyte@princeton.edu>
2017-04-22 17:01:02 -0400

    test

--- js/sell.js
+++ js/sell.js
@@ -17,10 +17,23 @@ app.controller("sellController", ["$scope", "$http", "$location",  function($sco
 	}
 
 
-	function mysubmit() {
-		console.log('test');
-   	 alert(clicked);
-	}
+	(function() {
+    $('form > input').keyup(function() {
+
+        var empty = false;
+        $('form > input').each(function() {
+            if ($(this).val() == '') {
+                empty = true;
+            }
+        });
+
+        if (empty) {
+            $('#submitButton').attr('disabled', 'disabled'); // updated according to http://stackoverflow.com/questions/7637790/how-to-remove-disabled-attribute-with-jquery-ie
+        } else {
+            $('#submitButton').removeAttr('disabled'); // updated according to http://stackoverflow.com/questions/7637790/how-to-remove-disabled-attribute-with-jquery-ie
+        }
+    });
+})()
 
 }])
 
