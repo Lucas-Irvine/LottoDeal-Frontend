e622d09e5ac16e1bd3cfe2bcea73434eb24f7c0b
Steven  Takeshita <Steven@nat-oitwireless-inside-vapornet100-d-8746.Princeton.EDU>
2017-04-23 14:47:08 -0400

    trying to make better

--- js/user.js
+++ js/user.js
@@ -23,7 +23,7 @@ function createReviewFunction() {
         var reviewDes = $("#reviewDes").val();
         var stars = $("#stars").val();
         var reviewerID = localStorage.getItem("curUserID");
-        if (stars < 6 && stars >= 0 && != null) {
+        if (stars < 6 && stars >= 0 && stars != null) {
             console.log("posting a review NOW!")
             data = {
              sellerID: sellerID,
