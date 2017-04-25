e883701d6b9b02d97221d4ef87d13c1993b86346
Lucas Irvine <lucasirvine@nat-oitwireless-inside-vapornet100-c-7561.Princeton.EDU>
2017-04-25 15:49:05 -0400

    front page changes

--- js/index.js
+++ js/index.js
@@ -649,3 +649,26 @@ app.controller("loginController", function($scope) {
     
 });
 
+$(document).ready(function()
+    {
+        $("#notifications").click(function()
+        {
+            $("#notificationContainer").fadeToggle(300);
+            $("#notification_count").fadeOut("slow");
+            return false;
+        });
+
+//Document Click hiding the popup
+        $(document).click(function()
+        {
+            $("#notificationContainer").hide();
+        });
+
+//Popup on click
+        $("#notificationContainer").click(function()
+        {
+            return false;
+        });
+
+    });
+
