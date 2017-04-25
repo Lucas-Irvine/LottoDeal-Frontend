6f9096be7cbffe7d5e0f830832e93adabe0c31e7
Steven  Takeshita <Steven@nat-oitwireless-inside-vapornet100-d-8746.Princeton.EDU>
2017-04-25 00:09:16 -0400

    adding login stuff to sell

--- js/facebooklogin.js
+++ js/facebooklogin.js
@@ -48,10 +48,12 @@ facebookLoginButton.onclick = function() {
             //display user data
             fbLogout()
             document.getElementById('successScreen').innerHTML = 'Thanks for Logging Out';
+            document.getElementById('login').innerHTML = 'Logout';
         } else {
             document.getElementById('loginTitle').innerHTML = 'Login';
             fbLogin()
             document.getElementById('successScreen').innerHTML = 'Thanks for Logging In';
+            document.getElementById('login').innerHTML = 'Login';
         }
     });
 }
