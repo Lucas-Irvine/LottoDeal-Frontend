1b65c38f90032060ea19e828d169ed3baf9ab0d5
Steven  Takeshita <Steven@nat-oitwireless-inside-vapornet100-d-8746.Princeton.EDU>
2017-04-26 23:49:49 -0400

    testing average user thing

--- js/index.js
+++ js/index.js
@@ -129,7 +129,7 @@ app.controller("indexController", ["$scope", "$rootScope", "$location", function
             var accounts = JSON.parse(data)
             
             $scope.accounts = accounts;
-            console.log($scope.accounts + "this is the account infomation")
+            console.log($scope.accounts)
             $scope.$apply()
         },
         error: function(response, error) {
