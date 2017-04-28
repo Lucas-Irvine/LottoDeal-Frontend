fce8e9444199b959f958d9b15a80272a5b899b0e
Steven  Takeshita <Steven@nat-oitwireless-inside-vapornet100-d-8746.Princeton.EDU>
2017-04-27 23:54:07 -0400

    hello

--- js/sell.js
+++ js/sell.js
@@ -1,7 +1,7 @@
 var app = angular.module("app", []);
 
 
-markRead();
+var markRead();
 
 app.controller("sellController", ["$scope", "$http", "$location",  function($scope, $http, $location) {
     console.log("got here")
