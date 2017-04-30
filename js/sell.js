0839449f1e49c3448acdbd7b0e5f9314492aa030
Steven  Takeshita <Steven@nat-oitwireless-inside-vapornet100-d-8746.Princeton.EDU>
2017-04-30 13:19:35 -0400

    fixing login things

--- js/sell.js
+++ js/sell.js
@@ -25,7 +25,7 @@ window.fbAsyncInit = function() {
             // Get and display the user profile data
         }
         else {
-            userID = null;
+            userID = undefined;
             console.log('Not logged in');
             document.getElementById('successScreen').innerHTML = "";
             document.getElementById('login').innerHTML = 'Login';
@@ -66,10 +66,17 @@ window.fbAsyncInit = function() {
 
 
 function saveUserID() {
-     FB.api('/me', {locale: 'en_US', fields: 'id'},
-        function (response) {
-            //localStorage.setItem("curUserID", response.id);
+   FB.api('/me', {locale: 'en_US', fields: 'id'},
+    function (response) {
+            //localStorage.setItem("curUserID", response.id)
+
             userID = response.id;
+
+
+            angular();
+
+
+            
             console.log(userID + "saving UserID as a global variable")
         });
 }
@@ -110,7 +117,7 @@ facebookLoginButton.onclick = function() {
 
 // when a user tries to sell an item check if they are logged in and open up a modal to login if they are not logged in
 function sellLoginCheck () {
-        FB.getLoginStatus(function(response) {
+    FB.getLoginStatus(function(response) {
         if (response.status === 'connected') {
             //display user data
             return true; 
@@ -142,7 +149,7 @@ function fbLogout() {
     //delete local storage
     // delete localStorage.curUserID;
 
-    userID = null;
+    userID = undefined;
 
     FB.logout(function() {
         console.log('Successfully logged out')
@@ -156,6 +163,11 @@ function getFbUserData(){
             userID = response.id;
             console.log(userID + "saving UserID as a global variable")
 
+
+
+            angular();
+
+
             // Save user data
             saveUserData(response);
         });
@@ -165,26 +177,26 @@ console.log(userID + "saving UserID as a global variable")
 
 function saveUserData(response) {
 
-      var url = "https://localhost:8000/createUser";
-      
-      console.log('Gender:' + response.gender);
-      console.log('Age max: ' + response.age_range.max);
-      console.log('Age min: ' + response.age_range.min);
+  var url = "https://localhost:8000/createUser";
+
+  console.log('Gender:' + response.gender);
+  console.log('Age max: ' + response.age_range.max);
+  console.log('Age min: ' + response.age_range.min);
       var avgAge = 25 //default to 25 if unspecified
 
       if (response.age_range != null) {
         avgAge = (response.age_range.max + response.age_range.min) / 2
         console.log('Avg age is' + avgAge);
-      }
+    }
 
-      data = {
+    data = {
         name: response.first_name+ ' ' + response.last_name,
         fbid: response.id,
         age: avgAge,
         gender: response.gender,
         url: 'http://graph.facebook.com/' + response.id + '/picture?type=large',
         email: response.email
-      }
+    }
 
       // AJAX POST TO SERVER
       $.ajax({
@@ -192,35 +204,34 @@ function saveUserData(response) {
         type: 'post',
         data: data,
         success: function(data) {
-        console.log(data)
+            console.log(data)
         },
         error: function(response, error) {
-        console.log(response)
-        console.log(error)
+            console.log(response)
+            console.log(error)
         }
     });
-}
+  }
 //End Facebook login code -----------------------------------
 
 
 
+function angular() {
 
 
 
+    var app = angular.module("app", []);
 
 
-var app = angular.module("app", []);
+    var scope;
 
 
-var scope;
+    app.controller("sellController", ["$scope", "$http", "$location",  function($scope, $http, $location) {
+        console.log("got here")
+        scope = $scope;
 
-
-app.controller("sellController", ["$scope", "$http", "$location",  function($scope, $http, $location) {
-    console.log("got here")
-    scope = $scope;
-    
-    var sellerID = userID;
-    console.log("hello its" + sellerID);
+        var sellerID = userID;
+        console.log("hello its" + sellerID);
 
     //var sellerID = localStorage.getItem("curUserID");
     $("#userid").val(sellerID)
@@ -243,7 +254,7 @@ app.controller("sellController", ["$scope", "$http", "$location",  function($sco
         $scope.id = id;
     }
     else {
-  		$("#invalidFormModal").modal();
+        $("#invalidFormModal").modal();
     }
 
 
@@ -257,7 +268,7 @@ app.controller("sellController", ["$scope", "$http", "$location",  function($sco
 
 
 // AJAX POST TO SERVER
-    var notificationUrl = "https://localhost:8000/getNotifications";
+var notificationUrl = "https://localhost:8000/getNotifications";
     //var userID = localStorage.getItem("curUserID")
     var dataGET = {
         userID: userID
@@ -271,10 +282,10 @@ app.controller("sellController", ["$scope", "$http", "$location",  function($sco
             var notifications = JSON.parse(data)
 
             var monthNames = [
-                "January", "February", "March",
-                "April", "May", "June", "July",
-                "August", "September", "October",
-                "November", "December"
+            "January", "February", "March",
+            "April", "May", "June", "July",
+            "August", "September", "October",
+            "November", "December"
             ];
 
             var curDate = new Date();
@@ -303,7 +314,7 @@ app.controller("sellController", ["$scope", "$http", "$location",  function($sco
                     var day = date.getDate();
 
                     /* code taken from http://stackoverflow.com/questions/8888491/
-                     how-do-you-display-javascript-datetime-in-12-hour-am-pm-format */
+                    how-do-you-display-javascript-datetime-in-12-hour-am-pm-format */
 
                     var hours = date.getHours();
                     var minutes = date.getMinutes();
@@ -338,54 +349,54 @@ app.controller("sellController", ["$scope", "$http", "$location",  function($sco
 	// mark all the notifications as read
 	scope.markRead = function() {
 		// AJAX POST TO SERVER
-	    var readurl = "https://localhost:8000/markRead";
+       var readurl = "https://localhost:8000/markRead";
 	    //var userID = localStorage.getItem("curUserID")
 	    var data = {
-	        userID: userID
-	    }
-	    console.log('Asking for notifications')
-	    $.ajax({
-	        url: readurl,
-	        data: data,
-	        type: 'GET',
-	        success: function(data) {
-	            var notifications = JSON.parse(data)
-	            $scope.notificationLength = 0;
+           userID: userID
+       }
+       console.log('Asking for notifications')
+       $.ajax({
+           url: readurl,
+           data: data,
+           type: 'GET',
+           success: function(data) {
+               var notifications = JSON.parse(data)
+               $scope.notificationLength = 0;
 
-                var monthNames = [
-                    "January", "February", "March",
-                    "April", "May", "June", "July",
-                    "August", "September", "October",
-                    "November", "December"
-                ];
+               var monthNames = [
+               "January", "February", "March",
+               "April", "May", "June", "July",
+               "August", "September", "October",
+               "November", "December"
+               ];
 
-                var curDate = new Date();
+               var curDate = new Date();
 
-                for (i = 0; i < notifications.length; i++) {
+               for (i = 0; i < notifications.length; i++) {
 
-                    var date = new Date(notifications[i].datePosted);
+                var date = new Date(notifications[i].datePosted);
 
-                    var hoursAgo = Math.abs(DateDiff.inHours(curDate, date));
+                var hoursAgo = Math.abs(DateDiff.inHours(curDate, date));
 
-                    if (hoursAgo < 24) {
-                        if (hoursAgo == 0) {
-                            notifications[i].datePosted = "Under an hour ago";
-                        }
-                        else if (hoursAgo == 1) {
-                            notifications[i].datePosted = hoursAgo + " hour ago";
-                        }
-                        else {
-                            notifications[i].datePosted = hoursAgo + " hours ago";
-                            console.log(notifications[i].datePosted);
-                        }
+                if (hoursAgo < 24) {
+                    if (hoursAgo == 0) {
+                        notifications[i].datePosted = "Under an hour ago";
                     }
-
+                    else if (hoursAgo == 1) {
+                        notifications[i].datePosted = hoursAgo + " hour ago";
+                    }
                     else {
-                        var month = date.getMonth();
-                        var day = date.getDate();
+                        notifications[i].datePosted = hoursAgo + " hours ago";
+                        console.log(notifications[i].datePosted);
+                    }
+                }
+
+                else {
+                    var month = date.getMonth();
+                    var day = date.getDate();
 
                         /* code taken from http://stackoverflow.com/questions/8888491/
-                         how-do-you-display-javascript-datetime-in-12-hour-am-pm-format */
+                        how-do-you-display-javascript-datetime-in-12-hour-am-pm-format */
 
                         var hours = date.getHours();
                         var minutes = date.getMinutes();
@@ -403,51 +414,51 @@ app.controller("sellController", ["$scope", "$http", "$location",  function($sco
                     }
                 }
 
-	            $scope.notifications = notifications;
-	            console.log($scope.notifications)
-	            console.log("updated the notifications")
-	            $scope.$apply()
-	        },
-	        error: function(response, error) {
-	            console.log(response)
-	            console.log(error)
-	        }
-	    });
-	}
+                $scope.notifications = notifications;
+                console.log($scope.notifications)
+                console.log("updated the notifications")
+                $scope.$apply()
+            },
+            error: function(response, error) {
+               console.log(response)
+               console.log(error)
+           }
+       });
+   }
 
 
-	$scope.debugSubmit = function() {
-		var testUrl = "https://localhost:8000/debugPost"
-		var sellerID = localStorage.getItem("curUserID")
+   $scope.debugSubmit = function() {
+      var testUrl = "https://localhost:8000/debugPost"
+      var sellerID = localStorage.getItem("curUserID")
 
-		var data = {
-			title: "Test Item",
-			price: "5",
-			longDescription: "This is a great item, with the following features: \
-			- Great \
-			- Awesome \
-			- Cool \
-			- Amazing \
-			You should buy it!",
-			shortDescription: "This is a short description for a great product.",
-			expirDate: "1",
-			userID: sellerID,
-		}
+      var data = {
+         title: "Test Item",
+         price: "5",
+         longDescription: "This is a great item, with the following features: \
+         - Great \
+         - Awesome \
+         - Cool \
+         - Amazing \
+         You should buy it!",
+         shortDescription: "This is a short description for a great product.",
+         expirDate: "1",
+         userID: sellerID,
+     }
 
-		$.ajax({
-		    url: testUrl,
-		    type: 'POST',
-		    data: data,
-		    success: function(data) {
-				console.log(data)
-		    },
-		    error: function(response, error) {
-				console.log(response)
-				console.log(error)
-		    }
-		});
-	}
-	
+     $.ajax({
+      url: testUrl,
+      type: 'POST',
+      data: data,
+      success: function(data) {
+        console.log(data)
+    },
+    error: function(response, error) {
+        console.log(response)
+        console.log(error)
+    }
+});
+ }
+
 	// //when field is entered
 	// $('#price, #title, #description').bind('keyup', function() {
 		
@@ -489,6 +500,8 @@ app.controller("sellController", ["$scope", "$http", "$location",  function($sco
 
 }])
 
+}
+
 /* code taken from http://stackoverflow.com/questions/12368910/html-display-image-after-selecting-filename */
 
 function readURL(input) {
@@ -497,9 +510,9 @@ function readURL(input) {
 
         reader.onload = function (e) {
             $('#imageUploaded')
-                .attr('src', e.target.result)
-                .width(150)
-                .height(100);
+            .attr('src', e.target.result)
+            .width(150)
+            .height(100);
         };
 
         reader.readAsDataURL(input.files[0]);
