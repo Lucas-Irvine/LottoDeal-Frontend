c135325005c46aa5cfbdc244c6d90576a2e74501
Lucas Irvine <lucasirvine@nat-oitwireless-inside-vapornet100-c-7561.Princeton.EDU>
2017-04-29 21:45:03 -0400

    index changes

--- js/index.js
+++ js/index.js
@@ -759,3 +759,39 @@ function base64ArrayBuffer(arrayBuffer) {
 //         document.getElementById('status').innerHTML = 'You have successfully logout from Facebook.';
 //     });
 // }
+
+/* For notifications */
+
+var check = false;
+
+$(document).ready(function() {
+    $("#notifications").click(function() {
+        if (!check) {
+            $("#notificationContainer").fadeIn(300);
+            // $("#notification_count").fadeOut("slow");
+            check = true;
+        }
+        else {
+            $("#notificationContainer").fadeOut(300);
+            scope.markRead();
+            check = false;
+        }
+
+        return false;
+    });
+
+    //Document Click hiding the popup
+    $(document).click(function() {
+        $("#notificationContainer").hide();
+        if (check) {
+            scope.markRead();
+        }
+        check = false;
+    });
+
+    // //Popup on click
+    // $("#notificationContainer").click(function() {
+    //     return false;
+    // });
+
+});
