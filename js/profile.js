25911922d925d513d595402c1167a4046570f2f0
Antony Toron <atoron@princeton.edu>
2017-04-30 05:00:23 -0400

    fixing images on user profile

--- js/profile.js
+++ js/profile.js
@@ -109,6 +109,33 @@ app.controller("profileController", ["$scope", "$rootScope", "$location", functi
                 var expirationDate = new Date(items[i].expirationDate);
                 var date = new Date();
                 items[i].expirationDate = DateDiff.inHours(date, expirationDate) + " Hours " + DateDiff.inDays(date, expirationDate) + " Days left";
+            
+                if (items[i].expired) {
+                    items[i].expirationDate = "Lottery has expired!";                     
+                }
+                else if (items[i].sold) {
+                    items[i].expirationDate = "Item was sold to:" + items[i].winnerName;
+                }
+                else if (hours < 0 || days < 0) {
+                     items[i].expirationDate = "Negative days remaining (not expired yet)";   
+                }
+                else {
+                    items[i].expirationDate =  hours + " Hours " + days + " Days left";
+                }
+               // items[i]["src"] = 'data:image/jpeg;base64,' + btoa(items[i].data.data)
+
+                var image = items[i].img;
+                if (image == null) {
+                    items[i]["src"] = "https://placeholdit.imgix.net/~text?txtsize=30&txt=320%C3%97150&w=320&h=150"
+                } 
+                else if (items[i].img.compressed != null) {
+                    items[i]["src"] = items[i].img.compressed;
+                }
+                else {
+                    var b64 = base64ArrayBuffer(items[i].img.data.data)
+                    var dataURL = "data:image/jpeg;base64," + b64;
+                    items[i]["src"] = dataURL;
+                }
             }
             $scope.items = items;
             console.log($scope.items)
@@ -139,6 +166,36 @@ app.controller("profileController", ["$scope", "$rootScope", "$location", functi
                 var expirationDate = new Date(items[i].expirationDate);
                 var date = new Date();
                 items[i].expirationDate = DateDiff.inHours(date, expirationDate) + " Hours " + DateDiff.inDays(date, expirationDate) + " Days left";
+            
+                if (items[i].expired) {
+                    items[i].expirationDate = "Lottery has expired!";                     
+                }
+                else if (items[i].sold) {
+                    items[i].expirationDate = "Item was sold to:" + items[i].winnerName;
+                }
+                else if (hours < 0 || days < 0) {
+                     items[i].expirationDate = "Negative days remaining (not expired yet)";   
+                }
+                else {
+                    items[i].expirationDate =  hours + " Hours " + days + " Days left";
+                }
+               // items[i]["src"] = 'data:image/jpeg;base64,' + btoa(items[i].data.data)
+
+                var image = items[i].img;
+                if (image == null) {
+                    items[i]["src"] = "https://placeholdit.imgix.net/~text?txtsize=30&txt=320%C3%97150&w=320&h=150"
+                } 
+                else if (items[i].img.compressed != null) {
+                    items[i]["src"] = items[i].img.compressed;
+                }
+                else {
+                    var b64 = base64ArrayBuffer(items[i].img.data.data)
+                    var dataURL = "data:image/jpeg;base64," + b64;
+                    items[i]["src"] = dataURL;
+                }
+
+
+
             }
             $scope.listedItems = items;
             console.log($scope.listedItems)
@@ -172,6 +229,32 @@ app.controller("profileController", ["$scope", "$rootScope", "$location", functi
                 var expirationDate = new Date(items[i].expirationDate);
                 var date = new Date();
                 items[i].expirationDate = DateDiff.inHours(date, expirationDate) + " Hours " + DateDiff.inDays(date, expirationDate) + " Days left";
+                if (items[i].expired) {
+                    items[i].expirationDate = "Lottery has expired!";                     
+                }
+                else if (items[i].sold) {
+                    items[i].expirationDate = "Item was sold to:" + items[i].winnerName;
+                }
+                else if (hours < 0 || days < 0) {
+                     items[i].expirationDate = "Negative days remaining (not expired yet)";   
+                }
+                else {
+                    items[i].expirationDate =  hours + " Hours " + days + " Days left";
+                }
+               // items[i]["src"] = 'data:image/jpeg;base64,' + btoa(items[i].data.data)
+
+                var image = items[i].img;
+                if (image == null) {
+                    items[i]["src"] = "https://placeholdit.imgix.net/~text?txtsize=30&txt=320%C3%97150&w=320&h=150"
+                } 
+                else if (items[i].img.compressed != null) {
+                    items[i]["src"] = items[i].img.compressed;
+                }
+                else {
+                    var b64 = base64ArrayBuffer(items[i].img.data.data)
+                    var dataURL = "data:image/jpeg;base64," + b64;
+                    items[i]["src"] = dataURL;
+                }
             }
             $scope.soldItems = items;
             console.log($scope.soldItems)
@@ -361,6 +444,70 @@ app.controller("profileController", ["$scope", "$rootScope", "$location", functi
     $scope.targetPost = null;
 
 }])
+
+// Converts an ArrayBuffer directly to base64, without any intermediate 'convert to string then
+// use window.btoa' step. According to my tests, this appears to be a faster approach:
+// http://jsperf.com/encoding-xhr-image-data/5
+
+/*
+MIT LICENSE
+Copyright 2011 Jon Leighton
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+function base64ArrayBuffer(arrayBuffer) {
+    var base64 = ''
+    var encodings = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+
+    var bytes = new Uint8Array(arrayBuffer)
+    var byteLength = bytes.byteLength
+    var byteRemainder = byteLength % 3
+    var mainLength = byteLength - byteRemainder
+
+    var a, b, c, d
+    var chunk
+
+    // Main loop deals with bytes in chunks of 3
+    for (var i = 0; i < mainLength; i = i + 3) {
+        // Combine the three bytes into a single integer
+        chunk = (bytes[i] << 16) | (bytes[i + 1] << 8) | bytes[i + 2]
+
+        // Use bitmasks to extract 6-bit segments from the triplet
+        a = (chunk & 16515072) >> 18 // 16515072 = (2^6 - 1) << 18
+        b = (chunk & 258048) >> 12 // 258048   = (2^6 - 1) << 12
+        c = (chunk & 4032) >> 6 // 4032     = (2^6 - 1) << 6
+        d = chunk & 63 // 63       = 2^6 - 1
+
+        // Convert the raw binary segments to the appropriate ASCII encoding
+        base64 += encodings[a] + encodings[b] + encodings[c] + encodings[d]
+    }
+
+    // Deal with the remaining bytes and padding
+    if (byteRemainder == 1) {
+        chunk = bytes[mainLength]
+
+        a = (chunk & 252) >> 2 // 252 = (2^6 - 1) << 2
+
+        // Set the 4 least significant bits to zero
+        b = (chunk & 3) << 4 // 3   = 2^2 - 1
+
+        base64 += encodings[a] + encodings[b] + '=='
+    } else if (byteRemainder == 2) {
+        chunk = (bytes[mainLength] << 8) | bytes[mainLength + 1]
+
+        a = (chunk & 64512) >> 10 // 64512 = (2^6 - 1) << 10
+        b = (chunk & 1008) >> 4 // 1008  = (2^6 - 1) << 4
+
+        // Set the 2 least significant bits to zero
+        c = (chunk & 15) << 2 // 15    = 2^4 - 1
+
+        base64 += encodings[a] + encodings[b] + encodings[c] + '='
+    }
+
+    return base64
+}
 
 //Code modified from http://ditio.net/2010/05/02/javascript-date-difference-calculation/
 var DateDiff = {
