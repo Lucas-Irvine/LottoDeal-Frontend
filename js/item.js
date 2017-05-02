4a03aff6e0841f03969d153d8c13dbf7e1e5347e
Steven  Takeshita <Steven@nat-oitwireless-inside-vapornet100-d-8746.Princeton.EDU>
2017-05-02 18:50:12 -0400

    adding login and notification and no bidding stuff to item

--- js/item.js
+++ js/item.js
@@ -9,6 +9,8 @@ $('#myTabs a').click(function(e) {
 });
 
 
+var userID;
+var scope;
 
 //Code modified from https://www.w3schools.com/howto/howto_js_tabs.asp
 function changeTab(titleID, id) {
@@ -45,6 +47,7 @@ app.controller("itemController", ["$scope", "$rootScope", "$location", "$routePa
     var id = searchObject['id'];
     console.log(id);
 
+    scope = $scope;
 
     //GET SUGGESTIONS
     var test = [{
@@ -91,10 +94,100 @@ app.controller("itemController", ["$scope", "$rootScope", "$location", "$routePa
 
     $scope.post = null;
 
+
+
+
+
+       var itemIDs = [];
+    // mark all the notifications as read
+    scope.markRead = function() {
+        // AJAX POST TO SERVER
+        var readurl = "https://localhost:8000/markRead";
+        // var userID = localStorage.getItem("curUserID")
+
+        console.log(userID + "here's the userID in mark read");
+        var data = {
+            userID: userID
+        }
+        console.log('Asking for notifications')
+        $.ajax({
+            url: readurl,
+            data: data,
+            type: 'GET',
+            success: function(data) {
+                var notifications = JSON.parse(data)
+                $scope.notificationLength = 0;
+
+                var monthNames = [
+                    "January", "February", "March",
+                    "April", "May", "June", "July",
+                    "August", "September", "October",
+                    "November", "December"
+                ];
+
+                var curDate = new Date();
+
+                for (i = 0; i < notifications.length; i++) {
+
+                    itemIDs.push(notifications[i].itemID);
+
+                    var date = new Date(notifications[i].datePosted);
+
+                    var hoursAgo = Math.abs(DateDiff.inHours(curDate, date));
+
+                    if (hoursAgo < 24) {
+                        if (hoursAgo == 0) {
+                            notifications[i].datePosted = "Just Now";
+                        }
+                        else if (hoursAgo == 1) {
+                            notifications[i].datePosted = hoursAgo + " hour ago";
+                        }
+                        else {
+                            notifications[i].datePosted = hoursAgo + " hours ago";
+                            console.log(notifications[i].datePosted);
+                        }
+                    }
+
+                    else {
+                        var month = date.getMonth();
+                        var day = date.getDate();
+
+                        /* code taken from http://stackoverflow.com/questions/8888491/
+                         how-do-you-display-javascript-datetime-in-12-hour-am-pm-format */
+
+                        var hours = date.getHours();
+                        var minutes = date.getMinutes();
+                        var ampm = hours >= 12 ? 'pm' : 'am';
+                        hours = hours % 12;
+                        hours = hours ? hours : 12; // the hour '0' should be '12'
+                        minutes = minutes < 10 ? '0'+ minutes : minutes;
+                        var strTime = hours + ':' + minutes + ' ' + ampm;
+
+                        /* --------- */
+
+                        var newDate = monthNames[month] + " " + day + " at " + strTime;
+                        notifications[i].datePosted = monthNames[month] + " " + day + " at " + strTime;
+                        console.log(newDate);
+                    }
+                }
+
+                $scope.notifications = notifications;
+                console.log($scope.notifications)
+                console.log("updated the notifications")
+                $scope.$apply()
+            },
+            error: function(response, error) {
+                console.log(response)
+                console.log(error)
+            }
+        });
+    }
+    
+
     var url = "https://localhost:8000/getItem"
 
-    var userid = localStorage.getItem("curUserID");
-    console.log(userid)
+    // var userid = localStorage.getItem("curUserID");
+    // console.log(userid)
 
     $scope.canEdit = false;
 
@@ -243,33 +336,42 @@ app.controller("itemController", ["$scope", "$rootScope", "$location", "$routePa
         $scope.itemID = itemID
         $scope.itemTitle = itemTitle
         $scope.amountRaised = amountRaised
-        if (price >= amountRaised + amount) {
-            handler.open({
-                name: 'LottoDeal',
-                description: 'Bid on ' + itemTitle,
-                amount: amount * 100
-            });
-        } else {
-            console.log('Bid overpasses item price!');
-            BootstrapDialog.show({
-                title: 'Bid surpasses item price',
-                message: 'Choose a lower bid or search for similar items',
-                buttons: [{
-                    id: 'btn-ok',
-                    icon: 'glyphicon glyphicon-check',
-                    label: 'OK',
-                    cssClass: 'btn-primary',
-                    data: {
-                        js: 'btn-confirm',
-                        'user-id': '3'
-                    },
-                    autospin: false,
-                    action: function(dialogRef) {
-                        dialogRef.close();
-                    }
-                }]
-            });
 
+        if (userID != undefined) {
+
+            if (price >= amountRaised + amount) {
+                handler.open({
+                    name: 'LottoDeal',
+                    description: 'Bid on ' + itemTitle,
+                    amount: amount * 100
+                });
+            } else {
+                console.log('Bid overpasses item price!');
+                BootstrapDialog.show({
+                    title: 'Bid surpasses item price',
+                    message: 'Choose a lower bid or search for similar items',
+                    buttons: [{
+                        id: 'btn-ok',
+                        icon: 'glyphicon glyphicon-check',
+                        label: 'OK',
+                        cssClass: 'btn-primary',
+                        data: {
+                            js: 'btn-confirm',
+                            'user-id': '3'
+                        },
+                        autospin: false,
+                        action: function(dialogRef) {
+                            dialogRef.close();
+                        }
+                    }]
+                });
+
+            }
+        }
+        else {
+                document.getElementById('loginMessage').innerHTML = 'You must login before you are able to bid on an item!';
+                showLoginPopup();
+                console.log('UserID is undefined')
         }
 
     }
@@ -440,3 +542,180 @@ function base64ArrayBuffer(arrayBuffer) {
 
     return base64
 }
+
+
+
+
+
+
+
+
+
+
+    // Facebook Login code -----------------------------------
+window.fbAsyncInit = function() {
+    FB.init({
+        appId      : '228917890846081',
+        xfbml      : true,
+        cookie     : true,
+        version    : 'v2.8'
+    });   
+
+    // Check whether the user already logged in
+    FB.getLoginStatus(function(response) {
+        if (response.status === 'connected') {
+            //display user data
+            document.getElementById('successScreen').innerHTML = "";
+            document.getElementById('login').innerHTML = 'Logout';
+            facebookLoginButton.innerHTML = "Sign Out With Facebook";
+            $("#signInMessage").hide();
+
+
+     FB.api('/me', {locale: 'en_US', fields: 'id'},
+        function (response) {
+            //localStorage.setItem("curUserID", response.id);
+            userID = response.id;
+            $("#userid").val(userID)
+            scope.getNotifications(userID);
+            console.log(userID + "saving UserID as a global variable when logging in ")
+        });
+
+            //saveUserID();
+            console.log('logged in')
+            // Get and display the user profile data
+        }
+        else {
+            userID = undefined;
+            console.log('Not logged in');
+            document.getElementById('successScreen').innerHTML = "";
+            document.getElementById('login').innerHTML = 'Login';
+            $("#signInMessage").show();
+            facebookLoginButton.innerHTML = "Sign In With Facebook";
+        }
+    });
+
+
+
+
+
+};
+
+(function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src="https://connect.facebook.net/en_US/all.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+
+
+function showLoginPopup() {
+    $('#loginPopup').modal({
+        keyboard: false
+    })
+};
+
+var facebookLoginButton = document.getElementById("loginToFacebook");
+
+// When the user clicks the button, open the modal 
+facebookLoginButton.onclick = function() {
+    console.log('logging in/out')
+    document.getElementById('loginMessage').innerHTML = ""
+    FB.getLoginStatus(function(response) {
+        if (response.status === 'connected') {
+            //display user data
+            fbLogout()
+
+            document.getElementById('successScreen').innerHTML = 'Thanks for Logging Out';
+            document.getElementById('login').innerHTML = 'Login';
+            facebookLoginButton.innerHTML = "Sign In With Facebook";
+            $("#signInMessage").show();
+        } else {
+            fbLogin()
+
+            document.getElementById('login').innerHTML = 'Logout';
+            facebookLoginButton.innerHTML = "Sign Out With Facebook";
+            $("#signInMessage").hide();
+        }
+    });
+}
+
+
+
+function fbLogin() {
+    var window = FB.login(function (response) {
+        if (response.authResponse) {
+            // Get and display the user profile data
+            document.getElementById('successScreen').innerHTML = 'Thanks for Logging In';
+            console.log('Successfully logged in')
+            getFbUserData();
+        } else {
+            document.getElementById('status').innerHTML = 'User cancelled login or did not fully authorize.';
+        }
+    }, {scope: 'email'});
+}
+
+// Logout from facebook
+function fbLogout() {
+    //delete local storage
+    // delete localStorage.curUserID;
+
+    userID = undefined;
+
+    FB.logout(function() {
+        console.log('Successfully logged out')
+    });
+}
+
+function getFbUserData(){
+    FB.api('/me', {locale: 'en_US', fields: 'id,first_name,last_name,email,link,gender,locale,picture, age_range'},
+        function (response) {
+            //localStorage.setItem("curUserID", response.id);
+            userID = response.id;
+            $("#userid").val(userID)
+            console.log(userID + "saving UserID as a global variable")
+
+            // Save user data
+            saveUserData(response);
+        });
+}
+
+function saveUserData(response) {
+
+      var url = "https://localhost:8000/createUser";
+      
+      console.log('Gender:' + response.gender);
+      console.log('Age max: ' + response.age_range.max);
+      console.log('Age min: ' + response.age_range.min);
+      var avgAge = 25 //default to 25 if unspecified
+
+      if (response.age_range != null) {
+        avgAge = (response.age_range.max + response.age_range.min) / 2
+        console.log('Avg age is' + avgAge);
+      }
+
+      data = {
+        name: response.first_name+ ' ' + response.last_name,
+        fbid: response.id,
+        age: avgAge,
+        gender: response.gender,
+        url: 'http://graph.facebook.com/' + response.id + '/picture?type=large',
+        email: response.email
+      }
+
+      // AJAX POST TO SERVER
+      $.ajax({
+        url: url,
+        type: 'post',
+        data: data,
+        success: function(data) {
+        console.log(data)
+        },
+        error: function(response, error) {
+        console.log(response)
+        console.log(error)
+        }
+    });
+}
+//End Facebook login code -----------------------------------
