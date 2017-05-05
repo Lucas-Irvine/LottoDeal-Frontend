a82d8e646e736e43bceeb032f9fdc60f9859b80d
Lucas Irvine <lucasirvine@nat-oitwireless-inside-vapornet100-c-7561.Princeton.EDU>
2017-05-05 03:33:37 -0400

    tab change

--- js/profile.js
+++ js/profile.js
@@ -1,10 +1,10 @@
-// /* JS for the tabs */
-// $(document).ready(function() {
-//     $(".btn-pref .btn").click(function() {
-//         $(".btn-pref .btn").removeClass("btn-primary").addClass("btn-default");
-//         $(this).removeClass("btn-default").addClass("btn-primary");
-//     });
-// });
+/* JS for the tabs */
+$(document).ready(function() {
+    $(".btn-pref .btn").click(function() {
+        $(".btn-pref .btn").removeClass("btn-primary").addClass("btn-default");
+        $(this).removeClass("btn-default").addClass("btn-primary");
+    });
+});
 
 
 // copied from http://stackoverflow.com/questions/46155/validate-email-address-in-javascript
