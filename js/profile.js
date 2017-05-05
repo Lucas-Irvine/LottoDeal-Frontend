6d91789d7dc960bccbc93be85912cc110dc12201
Lucas Irvine <lucasirvine@nat-oitwireless-inside-vapornet100-c-7561.Princeton.EDU>
2017-05-04 23:54:59 -0400

    profile

--- js/profile.js
+++ js/profile.js
@@ -51,7 +51,7 @@ function updateSettings() {
 var scope;
 
 
-var app = angular.module("profile_app", ["ngRoute"])
+var app = angular.module("profile_app", ["serverModule"])
 
 app.controller("profileController", ["$scope", "$rootScope", "$location", "serverGet", "serverPost", function($scope, $rootScope, $location, serverGet, serverPost) {
     $scope.selectedTab = 0
