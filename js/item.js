b23944b4ce6c6a69249dec80d72993513bd74e06
Steven  Takeshita <Steven@nat-oitwireless-inside-vapornet100-d-8746.Princeton.EDU>
2017-05-04 22:46:21 -0400

    trying to fix item

--- js/item.js
+++ js/item.js
@@ -42,45 +42,48 @@ app.controller("itemController", ["$scope", "$location", "serverGet", "serverPos
 
     scope = $scope;
 
-    window.fbAsyncInit = function() {
 
-        FB.init({
-            appId: '228917890846081',
-            status: true,
-            cookie: true,
-            xfbml: true
-        });
-        FB.getLoginStatus(function(response) {
-            userID = response.authResponse.userID;
-            sellerID = sellerID;
-            // check if user can edit
-            if (sellerID == userID) {
-                console.log("matches")
-                $scope.canEdit = true;
 
-            } else {
-                console.log(userID + "cannot edit this post");
-            }
 
-            accessToken = response.authResponse.accessToken;
-            console.log('setting the access token to: ' + accessToken)
-            console.log(response);
-            console.log(userID + "saving UserID as a global variable when logging in ")
-            getSuggestions();
-        });
-    };
+    // window.fbAsyncInit = function() {
 
-    (function(d) {
-        var js, id = 'facebook-jssdk';
-        if (d.getElementById(id)) {
-            return;
-        }
-        js = d.createElement('script');
-        js.id = id;
-        js.async = true;
-        js.src = "//connect.facebook.net/en_US/all.js";
-        d.getElementsByTagName('head')[0].appendChild(js);
-    }(document));
+    //     FB.init({
+    //         appId: '228917890846081',
+    //         status: true,
+    //         cookie: true,
+    //         xfbml: true
+    //     });
+    //     FB.getLoginStatus(function(response) {
+    //         userID = response.authResponse.userID;
+    //         sellerID = sellerID;
+    //         // check if user can edit
+    //         if (sellerID == userID) {
+    //             console.log("matches")
+    //             $scope.canEdit = true;
+
+    //         } else {
+    //             console.log(userID + "cannot edit this post");
+    //         }
+
+    //         accessToken = response.authResponse.accessToken;
+    //         console.log('setting the access token to: ' + accessToken)
+    //         console.log(response);
+    //         console.log(userID + "saving UserID as a global variable when logging in ")
+    //         getSuggestions();
+    //     });
+    // };
+
+    // (function(d) {
+    //     var js, id = 'facebook-jssdk';
+    //     if (d.getElementById(id)) {
+    //         return;
+    //     }
+    //     js = d.createElement('script');
+    //     js.id = id;
+    //     js.async = true;
+    //     js.src = "//connect.facebook.net/en_US/all.js";
+    //     d.getElementsByTagName('head')[0].appendChild(js);
+    // }(document));
 
     function getSuggestions() {
         serverGet.getSuggestions(accessToken, $scope)
