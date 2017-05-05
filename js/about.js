0878ec5dc90d3072c9b688191bc6b6d638a9dd93
Lucas Irvine <lucasirvine@nat-oitwireless-inside-vapornet100-c-7561.Princeton.EDU>
2017-05-04 22:42:36 -0400

    about controller changes

---
+++ js/about.js
@@ -0,0 +1,16 @@
+app.controller("aboutController", ["$scope", "$rootScope", "$location", "serverGet", "serverPost", function($scope, $rootScope, $location, serverGet, serverPost) {
+
+    scope = $scope;
+    $scope.selectedTab = 0
+
+    scope.getNotifications = function (userID) {
+        serverGet.getNotifications(accessToken, $scope);
+    }
+
+    $scope.images = []
+
+// mark all the notifications as read
+    scope.markRead = function () {
+        serverGet.markRead(accessToken, $scope);
+    }
+}
