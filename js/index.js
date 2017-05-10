9def1f3a84ed4abfe75b09ab555173018d4c655d
Antony Toron <atoron@princeton.edu>
2017-05-09 22:18:41 -0400

    making notification look better

--- js/index.js
+++ js/index.js
@@ -109,134 +109,80 @@ app.controller("indexController", ["$scope", "$rootScope", "$location", "serverG
 
 
 
-                        for( var i = 0; i < 300; i++){
+            for( var i = 0; i < 300; i++){
 
-            particle.push({
-                x : width/2,
-                y : height/2,
-                boxW : randomRange(5,20),
-                boxH : randomRange(5,20),
-                size : randomRange(2,8),
-                
-                spikeran:randomRange(3,5),
-                
-                velX :randomRange(-8,8),
-                velY :randomRange(-50,-10),
-                
-                angle :convertToRadians(randomRange(0,360)),
-                color:colors[Math.floor(Math.random() * colors.length)],
-                anglespin:randomRange(-0.2,0.2),
-                
-                draw : function(){
-                
-                
-                    context.save();
+                particle.push({
+                    x : width/2,
+                    y : height/2,
+                    boxW : randomRange(5,20),
+                    boxH : randomRange(5,20),
+                    size : randomRange(2,8),
+                    
+                    spikeran:randomRange(3,5),
+                    
+                    velX :randomRange(-8,8),
+                    velY :randomRange(-50,-10),
+                    
+                    angle :convertToRadians(randomRange(0,360)),
+                    color:colors[Math.floor(Math.random() * colors.length)],
+                    anglespin:randomRange(-0.2,0.2),
+                    
+                    draw : function(){
+                        context.save();
                         context.translate(this.x,this.y);
                         context.rotate(this.angle);
-                    context.fillStyle=this.color;
-                    context.beginPath();
-                    // drawStar(0, 0, 5, this.boxW, this.boxH);
-                context.fillRect(this.boxW/2*-1,this.boxH/2*-1,this.boxW,this.boxH);
-                    context.fill();
-                            context.closePath();
-                    context.restore();
-                this.angle += this.anglespin;
-                this.velY*= 0.999;
-                this.velY += 0.3;
-              
-                this.x += this.velX;
-                this.y += this.velY;
-                    if(this.y < 0){
-                this.velY *= -0.2;
-                    this.velX *= 0.9;
-                };
-                if(this.y > height){
-                this.anglespin = 0;
-                this.y = height;
-                this.velY *= -0.2;
-                    this.velX *= 0.9;
-                };
-                    if(this.x > width ||this.x< 0){
-
-                this.velX *= -0.5;
-                };
-                
-                
-                
-                },
-
-
-
+                        context.fillStyle=this.color;
+                        context.beginPath();
+                        // drawStar(0, 0, 5, this.boxW, this.boxH);
+                        context.fillRect(this.boxW/2*-1,this.boxH/2*-1,this.boxW,this.boxH);
+                        context.fill();
+                        context.closePath();
+                        context.restore();
+                        this.angle += this.anglespin;
+                        this.velY*= 0.999;
+                        this.velY += 0.3;
+                      
+                        this.x += this.velX;
+                        this.y += this.velY;
+                        if(this.y < 0){
+                            this.velY *= -0.2;
+                            this.velX *= 0.9;
+                        };
+                        if(this.y > height){
+                            this.anglespin = 0;
+                            this.y = height;
+                            this.velY *= -0.2;
+                            this.velX *= 0.9;
+                        };
+                        if(this.x > width ||this.x< 0){
+                            this.velX *= -0.5;
+                        };
                     
+                    },
                 });
 
             }
-            // r1={
-            // x:width/2-150,
-            // y:height/2-150,
-            // width:300,
-            // height:300,
-            //     velX :0,
-            //     velY :-10,
-            //  img : loadImage("http://image.noelshack.com/fichiers/2015/12/1427051642-smiley.png"),
-            //  alphatop:0
-            // };
 
-
-            function drawScreen(){
-            // size = 50;
-            //             pFontName = "Lucida Sans Unicode";
-            //             context.font = size + "pt " + pFontName;
-            //             context.fillText("Confetti party !!!", width/2,150 );
-            //         if(r1.alphatop < 1){
-            //         r1.alphatop += 0.01;
-            //         }else{
-            //         r1.alphatop = 1;
-            //         }
-            //         context.globalAlpha = r1.alphatop; 
-            //             context.drawImage(r1.img,r1.x,r1.y);
-            //                   context.textAlign = 'center';
-
-                        
-                        
-            //             if(r1.alphatop === 1){
-            //     r1.velY*= 0.999;
-            //     r1.velY += 0.3;
-              
-            //     r1.x += r1.velX;
-            //     r1.y += r1.velY;}
-
-            //         if(r1.y + r1.height > height){
-            //     r1.anglespin = 0;
-            //     r1.y = height - r1.height;
-            //     r1.velY *= -0.8;
-            //         r1.velX *= 0.9;
-            //     };
-                
-                
-                        context.globalAlpha = 1; 
-                    for( var i = 0; i < particle.length; i++){
-                        particle[i].draw();
-
-                            }
-                            
-                            
+            function drawScreen(){                
+                context.globalAlpha = 1; 
+                for( var i = 0; i < particle.length; i++){
+                    particle[i].draw();
+                }
+                $("#canvas").hide();
             }
 
-                function loadImage(url){
-                    var img = document.createElement("img");
-                    img.src=url;
-                    return img;
-                }
+            function loadImage(url){
+                var img = document.createElement("img");
+                img.src=url;
+                return img;
+            }
 
             function update(){
+                context.clearRect(0,0,width,height);
 
+                drawScreen();
 
-            context.clearRect(0,0,width,height);
-
-            drawScreen();
-
-            requestAnimationFrame(update);
+                requestAnimationFrame(update);
             }
 
             update();
@@ -250,11 +196,11 @@ app.controller("indexController", ["$scope", "$rootScope", "$location", "serverG
                 return Math.floor(min + Math.random()* (max - min + 1));
             }
 
-                   function convertToRadians(degree) {
-                        return degree*(Math.PI/180);
-                    }
+           function convertToRadians(degree) {
+                return degree*(Math.PI/180);
+            }
                     
-                    function drawStar(cx, cy, spikes, outerRadius, innerRadius,color) {
+            function drawStar(cx, cy, spikes, outerRadius, innerRadius,color) {
                 var rot = Math.PI / 2 * 3;
                 var x = cx;
                 var y = cy;
@@ -282,9 +228,6 @@ app.controller("indexController", ["$scope", "$rootScope", "$location", "serverG
             }       
 
 
-
-
-            
             // $scope.winner = winner;
             // $scope.$apply();
         }, 3000)
