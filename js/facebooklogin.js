6cee7feb5b19945ea695f2ca457990e8bd00b648
Antony Toron <atoron@princeton.edu>
2017-05-13 21:16:09 -0400

    reverting changes for facebook login change on item page

--- js/facebooklogin.js
+++ js/facebooklogin.js
@@ -251,12 +251,8 @@ function getFbUserData() {
                 scope.getNotifications(accessToken);
             }
 
-            if (window.location.href.indexOf("item.html") > -1) {
-                window.location.href = window.location.href + "?id"
-            }
-            else {
-                window.location.reload(true); // reload a page when you log in
-            }
+            window.location.reload(true); // reload a page when you log in
+            
 
             // Save user data
             saveUserData(response);
